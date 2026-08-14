f550a3f1f6a6e81d0d7ce99e02405cecd7a16f17
Tokiniaina RAKOTOZANANY <hei.tokiniaina.2@gmail.com>
2026-08-14 10:14:39 +0300

    fix/grade repository wrong generic type
    * Fix GradeRepository generic type to Grade
    
    * Add Table annotation to User entity

--- src/main/java/hei/school/exam/repository/GradeRepository.java
+++ src/main/java/hei/school/exam/repository/GradeRepository.java
@@ -1,8 +1,9 @@
 package hei.school.exam.repository;
 
+import hei.school.exam.entity.Grade;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GradeRepository extends JpaRepository<GradeRepository, UUID> {}
+public interface GradeRepository extends JpaRepository<Grade, UUID> {}
