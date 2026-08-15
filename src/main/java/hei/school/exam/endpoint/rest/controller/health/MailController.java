f8d5224b59dd2764c09d0c8ba1ae39b636ba83a3
Tokiniaina RAKOTOZANANY <hei.tokiniaina.2@gmail.com>
2026-08-15 15:16:12 +0300

    feat: add mail sending base config

---
+++ src/main/java/hei/school/exam/endpoint/rest/controller/health/MailController.java
@@ -0,0 +1,27 @@
+package hei.school.exam.endpoint.rest.controller.health;
+
+import hei.school.exam.endpoint.event.EventProducer;
+import hei.school.exam.endpoint.event.model.SendMailRequested;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class MailController {
+    private final EventProducer<SendMailRequested> eventProducer;
+
+    @GetMapping("/hello")
+    @SneakyThrows
+    public String helloWorld(@RequestParam String to) {
+        var event = SendMailRequested.builder()
+                .to(to)
+                .build();
+        eventProducer.accept(List.of(event));
+        return "... world!";
+    }
+}
---
+++ src/main/java/hei/school/exam/service/event/SendMailRequestedService.java
@@ -0,0 +1,31 @@
+package hei.school.exam.service.event;
+
+import hei.school.exam.endpoint.event.model.SendMailRequested;
+import hei.school.exam.mail.Email;
+import hei.school.exam.mail.Mailer;
+import jakarta.mail.internet.InternetAddress;
+import java.util.List;
+import java.util.function.Consumer;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class SendMailRequestedService implements Consumer<SendMailRequested> {
+    private final Mailer mailer;
+
+    @SneakyThrows
+    @Override
+    public void accept(SendMailRequested sendEmailRequested) {
+        var recipientAddress = new InternetAddress(sendEmailRequested.getTo());
+        mailer.accept(
+                new Email(
+                        recipientAddress,
+                        List.of(),
+                        List.of(),
+                        "",
+                        "... world!",
+                        List.of())); //pièce jointe, ici ajouter le pdf
+    }
+}
