8cf54129c2dd06206630e1652ea4b1faaf2847a7
Tokiniaina RAKOTOZANANY <hei.tokiniaina.2@gmail.com>
2026-08-20 19:56:44 +0300

    feat: make Thymeleaf page public for demonstration

--- src/main/java/hei/school/exam/security/SecurityConfig.java
+++ src/main/java/hei/school/exam/security/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(
             auth ->
-                auth.requestMatchers("/login", "/ping").permitAll().anyRequest().authenticated())
+                auth.requestMatchers("/login", "/ping", "/").permitAll().anyRequest().authenticated())
         .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
     return http.build();
   }
